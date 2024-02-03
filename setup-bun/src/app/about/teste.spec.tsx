f72eb3e7162bd876536ac022067186a2d50052d5
Marcelo <mrcelo014@gmail.com>
2024-02-02 22:46:53 -0300

    bun checked

---
+++ setup-bun/src/app/about/teste.spec.tsx
@@ -0,0 +1,9 @@
+import { render } from '@testing-library/react'
+import Teste from './page'
+
+describe('teste', () => {
+  it('teste', () => {
+    expect(1).toBe(1)
+    render(<Teste />)
+  })
+})
